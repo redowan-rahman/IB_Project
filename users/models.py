e4e9cf972524b8f565d8f465b92482adce28898a
redowan-rahman <redowanudoy143@gmail.com>
2025-04-07 04:13:53 -0400

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +9,18 @@ class Profile(models.Model):
     about = models.TextField(blank=True)
     job = models.CharField(max_length=150, blank=True)
     profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+
+class RegisterForm(UserCreationForm):
+    firstname = forms.CharField(max_length=255)
+    lastname = forms.CharField(max_length=255)
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'firstname', 'lastname', 'email', 'password1', 'password2']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['phone', 'about', 'job', 'profile_image']
+        
