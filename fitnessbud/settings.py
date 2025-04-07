e3eda351e0cf6ff7bc5829310dd657e36ce6b0af
redowan-rahman <redowanudoy143@gmail.com>
2025-04-07 02:09:13 -0400

    Update settings.py

--- fitnessbud/settings.py
+++ fitnessbud/settings.py
@@ -137,9 +137,9 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / "static"
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
