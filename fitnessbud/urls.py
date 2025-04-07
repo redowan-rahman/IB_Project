22718512ba0737accaf68634723b344b0ab26a55
redowan-rahman <redowanudoy143@gmail.com>
2025-04-07 05:44:03 -0400

    Update urls.py

--- fitnessbud/urls.py
+++ fitnessbud/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
