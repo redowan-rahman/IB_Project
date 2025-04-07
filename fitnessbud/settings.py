350cffac3d0ff9d833d930d735db9e7c68c80306
redowan-rahman <redowanudoy143@gmail.com>
2025-04-07 05:11:37 -0400

    Update settings.py

--- fitnessbud/settings.py
+++ fitnessbud/settings.py
@@ -143,8 +143,8 @@ STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_ROOT = BASE_DIR / '/static/media/'
-MEDIA_URL = '/vol/web/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
